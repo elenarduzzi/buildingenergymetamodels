8b3d4c9099ef2d058a244222e104ce258cb949c0
elenarduzzi <E.R.Lenarduzzi@student.tudelft.nl>
2025-10-05 23:15:11 +0200

    Update 5B_split_vertex_multi.py

--- 5B_split_vertex_multi.py
+++ 5B_split_vertex_multi.py
@@ -2,7 +2,6 @@ import pandas as pd
 from pathlib import Path
 from tqdm import tqdm
 
-
 # combined veretx file should be the same for 2002, 2050, 2080, same pands removed from dataset. 
 # paths
 SPLITS = ["train", "test", "validate"]
@@ -18,12 +17,12 @@ for split in SPLITS:
     output_csv.parent.mkdir(parents=True, exist_ok=True)
     
     print(f"\nProcessing {split} ...")
-    # 1. Load Pand IDs as strings
+    # load Pand IDs as strings
     pand_ids = pd.read_csv(features_csv, usecols=["Pand ID"], dtype={"Pand ID": "string"})["Pand ID"]
     pand_set = set(pand_ids)
     print(f"Loaded {len(pand_set):,} target Pand IDs for {split}.")
 
-    # 2. Stream-filter the surfaces CSV
+    # stream-filter the surfaces CSV
     first_write = True
     with tqdm(desc=f"Filtering surface rows ({split})", unit="rows") as pbar:
         for chunk in pd.read_csv(VERTEX_CSV, dtype={"Pand ID": "string"}, chunksize=CHUNK):
